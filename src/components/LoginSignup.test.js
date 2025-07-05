645ee621adb7b1f3752c02da823ab50aa0751793
Tushar <94301511+tusharsingh3@users.noreply.github.com>
2025-07-05 23:30:56 +0530

    Delete src directory

--- src/components/LoginSignup.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-
-// Simple test that checks basic functionality
-test('components render without crashing', () => {
-  // This test just ensures the basic setup works
-  expect(true).toBe(true);
-});
